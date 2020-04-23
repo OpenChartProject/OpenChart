1cc43c333e591b2f9aaed85fa355528bf4f59b80
Jesse Roux <roux.jesse@gmail.com>
2020-04-22 23:18:56 -0400

    Remove unnecessary tests

--- OpenChart.Tests/src/Formats/OpenChart/TestOpenChartSerializer.cs
+++ OpenChart.Tests/src/Formats/OpenChart/TestOpenChartSerializer.cs
@@ -3,7 +3,6 @@ using OpenChart.Charting.Properties;
 using OpenChart.Formats;
 using OpenChart.Formats.OpenChart.Version0_1;
 using OpenChart.Formats.OpenChart.Version0_1.Data;
-using System.Text;
 
 namespace OpenChart.Tests.Formats.OpenChart
 {
@@ -24,76 +23,11 @@ namespace OpenChart.Tests.Formats.OpenChart
             serializer = new OpenChartSerializer();
         }
 
-        [TestCase("{}")]
-        [TestCase("{ \"metadata\": null }")]
-        public void Test_Deserialize_MetadataRequired(string data)
+        [Test]
+        public void Test_Serialize_VersionRequired()
         {
-            Assert.Throws<SerializerException>(() => serializer.Deserialize(Encoding.UTF8.GetBytes(data)));
-        }
-
-        [TestCase("{ \"metadata\": {} }")]
-        [TestCase("{ \"metadata\": { \"version\": null } }")]
-        [TestCase("{ \"metadata\": { \"version\": \"\" } }")]
-        public void Test_Deserialize_VersionRequired(string data)
-        {
-            Assert.Throws<SerializerException>(() => serializer.Deserialize(Encoding.UTF8.GetBytes(data)));
-        }
-
-        [TestCase(@"
-            {
-                ""metadata"": {
-                    ""version"": ""test-version""
-                }
-            }
-            ")]
-        [TestCase(@"
-            {
-                ""metadata"": {
-                    ""version"": ""test-version""
-                },
-                ""charts"": [],
-                ""song"": null
-            }
-            ")]
-        public void Test_Deserialize_EmptyProject(string data)
-        {
-            var pd = serializer.Deserialize(Encoding.UTF8.GetBytes(data));
-
-            Assert.AreEqual("test-version", pd.Metadata.Version);
-            Assert.IsEmpty(pd.Charts);
-            Assert.IsNull(pd.Song);
-        }
-
-        [TestCase(@"
-            {
-                ""metadata"": {
-                    ""version"": ""test-version""
-                },
-                ""charts"": [
-                    {
-                        ""keyCount"": 4,
-                        ""author"": ""jessie"",
-                        ""chartName"": ""test chart"",
-                        ""rows"": [],
-                        ""bpms"": []
-                    }
-                ],
-                ""song"": null
-            }
-            ")]
-        public void Test_Deserialize_EmptyChart(string data)
-        {
-            var pd = serializer.Deserialize(Encoding.UTF8.GetBytes(data));
-
-            Assert.AreEqual(1, pd.Charts.Length);
-
-            var chart = pd.Charts[0];
-
-            Assert.AreEqual(4, chart.KeyCount.Value);
-            Assert.AreEqual("jessie", chart.Author);
-            Assert.AreEqual("test chart", chart.ChartName);
-            Assert.IsEmpty(chart.BPMs);
-            Assert.IsEmpty(chart.Rows);
+            var project = new ProjectData();
+            Assert.Throws<SerializerException>(() => serializer.Serialize(project));
         }
 
         [Test]
