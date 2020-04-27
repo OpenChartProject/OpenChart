0120018f066f33bf551edfbda4210698b6fea70b
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-04-26 21:20:12 -0400

    Handle conversion between OpenChart chart objects and native chart objects

--- OpenChart/src/Charting/Objects/HoldNote.cs
+++ OpenChart/src/Charting/Objects/HoldNote.cs
@@ -1,4 +1,5 @@
 using OpenChart.Charting.Properties;
+using System;
 
 namespace OpenChart.Charting.Objects
 {
@@ -14,5 +15,22 @@ namespace OpenChart.Charting.Objects
         /// <param name="beat">The beat the hold note starts.</param>
         /// <param name="length">The length of the hold note (in beats).</param>
         public HoldNote(KeyIndex key, Beat beat, BeatDuration length) : base(key, beat, length) { }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is HoldNote note)
+                return (
+                    KeyIndex.Equals(note.KeyIndex) &&
+                    Beat.Equals(note.Beat) &&
+                    Length.Equals(note.Length)
+                );
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(KeyIndex, Beat, Length).GetHashCode();
+        }
     }
 }
