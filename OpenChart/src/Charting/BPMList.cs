68e3b3dcb251fe1c72926c3de8e4a055bee74fed
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-04-29 15:29:26 -0400

    Add comments

--- OpenChart/src/Charting/BPMList.cs
+++ OpenChart/src/Charting/BPMList.cs
@@ -2,11 +2,25 @@ using OpenChart.Charting.Properties;
 
 namespace OpenChart.Charting
 {
+    /// <summary>
+    /// Represents a list of BPM objects that is bundled with a time tracker.
+    /// </summary>
     public class BPMList
     {
+        /// <summary>
+        /// The list of BPM changes.
+        /// </summary>
         public BeatObjectList<BPM> BPMs { get; private set; }
+
+        /// <summary>
+        /// A time tracker. This is setup to watch the BPMs object for changes.
+        /// </summary>
         public BPMTimeTracker Time { get; private set; }
 
+        /// <summary>
+        /// Creates a new BPMList instance.
+        /// </summary>
+        /// <param name="bpms"></param>
         public BPMList(BeatObjectList<BPM> bpms)
         {
             BPMs = bpms;
