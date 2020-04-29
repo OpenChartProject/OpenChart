f06819b19f10a8e535fd00abc9ec2ef0a7606137
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-04-29 15:29:26 -0400

    Add comments

--- OpenChart/src/Charting/BPMTimeTracker.cs
+++ OpenChart/src/Charting/BPMTimeTracker.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 
 namespace OpenChart.Charting
 {
+    /// <summary>
+    /// Represents a BPM change at a specific point in time.
+    /// </summary>
     public class BPMInterval
     {
+        /// <summary>
+        /// The BPM of this interval.
+        /// </summary>
         public readonly BPM BPM;
+
+        /// <summary>
+        /// The time this BPM changed occurred.
+        /// </summary>
         public readonly double Seconds;
 
+        /// <summary>
+        /// Creates a new BPMInterval instance.
+        /// </summary>
         public BPMInterval(BPM bpm, double seconds)
         {
             BPM = bpm;
@@ -15,18 +28,24 @@ namespace OpenChart.Charting
         }
     }
 
+    /// <summary>
+    /// Generates a list of BPMInterval objects for a list of BPM objects. The tracker listens
+    /// to a BeatObjectList for changes so it knows when to regenerate the intervals.
+    /// </summary>
     public class BPMTimeTracker
     {
-        BeatObjectList<BPM> watchList;
         bool hasChanged;
-
         BPMInterval[] _intervals;
+
+        /// <summary>
+        /// An array of intervals. This automatically updates itself when the object list has changed.
+        /// </summary>
         public BPMInterval[] Intervals
         {
             get
             {
                 if (hasChanged)
-                    Update();
+                    updateIntervals();
 
                 return _intervals;
             }
@@ -36,18 +55,34 @@ namespace OpenChart.Charting
             }
         }
 
-        public BPMTimeTracker(BeatObjectList<BPM> watchList)
+        /// <summary>
+        /// The object list of BPMs the tracker is using to generate intervals.
+        /// </summary>
+        public BeatObjectList<BPM> ObjectList { get; private set; }
+
+        /// <summary>
+        /// Creates a new BPMTimeTracker instance.
+        /// </summary>
+        /// <param name="objectList">The BeatObjectList that will be watched for changes.</param>
+        public BPMTimeTracker(BeatObjectList<BPM> objectList)
         {
             hasChanged = true;
+            ObjectList = objectList;
 
-            watchList.Added += delegate { hasChanged = true; };
-            watchList.Cleared += delegate { hasChanged = true; };
-            watchList.Removed += delegate { hasChanged = true; };
+            // Flag the intervals as needing to be changed when the list updates.
+            objectList.Added += delegate { hasChanged = true; };
+            objectList.Cleared += delegate { hasChanged = true; };
+            objectList.Removed += delegate { hasChanged = true; };
         }
 
-        public void Update()
+        /// <summary>
+        /// Updates the Intervals array to reflect the new state of the object list.
+        /// This is only triggered when a caller tries to retrieve the Intervals property
+        /// and there has been a change since the last retrieval.
+        /// </summary>
+        private void updateIntervals()
         {
-            var bpms = watchList.ToArray();
+            var bpms = ObjectList.ToArray();
 
             BPMInterval curInterval = null;
             BPMInterval lastInterval = null;
@@ -59,8 +94,12 @@ namespace OpenChart.Charting
                     curInterval = new BPMInterval(bpm, 0);
                 else
                 {
+                    // Calculate how much time has elapsed since the last BPM change.
                     var beatDelta = bpm.Beat.Value - lastInterval.BPM.Beat.Value;
                     var timeDelta = beatDelta * lastInterval.BPM.SecondsPerBeat();
+
+                    // Add the elapsed time to track the total time needed to reach this
+                    // BPM change if you started at beat 0.
                     var elapsed = lastInterval.Seconds + timeDelta;
 
                     curInterval = new BPMInterval(lastInterval.BPM, elapsed);
