36a3346769e5759bbc83add0b6975ef29178523b
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-02 18:28:29 -0400

    Move BPMInterval into its own file

--- OpenChart/src/Charting/BPMIntervalTracker.cs
+++ OpenChart/src/Charting/BPMIntervalTracker.cs
@@ -6,55 +6,6 @@ using System.Collections.Generic;
 namespace OpenChart.Charting
 {
     /// <summary>
-    /// Represents a BPM change at a specific point in time.
-    /// </summary>
-    public class BPMInterval
-    {
-        /// <summary>
-        /// The BPM of this interval.
-        /// </summary>
-        public readonly BPM BPM;
-
-        /// <summary>
-        /// The time this BPM changed occurred.
-        /// </summary>
-        public readonly Time Time;
-
-        /// <summary>
-        /// Creates a new BPMInterval instance.
-        /// </summary>
-        public BPMInterval(BPM bpm, Time time)
-        {
-            BPM = bpm;
-            Time = time;
-        }
-
-        /// <summary>
-        /// Returns the beat that occurs at the given time for this BPM interval.
-        /// </summary>
-        /// <param name="targetTime">The time of the beat.</param>
-        public Time BeatToTime(Beat beat)
-        {
-            if (beat.Value < BPM.Beat.Value)
-                throw new ArgumentOutOfRangeException("The beat must be after the BPM change occurs.");
-
-            return Time.Value + (beat.Value - BPM.Beat.Value) * BPM.SecondsPerBeat();
-        }
-
-        /// <summary>
-        /// Returns the beat that occurs at the given time for this BPM interval.
-        /// </summary>
-        /// <param name="targetTime">The time of the beat.</param>
-        public Beat TimeToBeat(Time targetTime)
-        {
-            if (targetTime.Value < Time.Value)
-                throw new ArgumentOutOfRangeException("The time must be after the BPM change occurs.");
-
-            return BPM.Beat.Value + (targetTime.Value - Time.Value) * BPM.BeatsPerSecond();
-        }
-    }
-
-    /// <summary>
     /// Generates a list of BPMInterval objects for a list of BPM objects. The tracker listens
     /// to a BeatObjectList for changes so it knows when to regenerate the intervals.
     /// </summary>
