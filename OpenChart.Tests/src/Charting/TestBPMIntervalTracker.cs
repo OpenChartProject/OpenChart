a78c3c8788c981a27325006f0e070a211685fc84
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-02 14:25:50 -0400

    Add some interval tracker tests

--- OpenChart.Tests/src/Charting/TestBPMIntervalTracker.cs
+++ OpenChart.Tests/src/Charting/TestBPMIntervalTracker.cs
@@ -9,13 +9,13 @@ namespace OpenChart.Tests.Charting
     public class TestBPMIntervalTracker
     {
         [Test]
-        public void Test_Init_CannotBeNull()
+        public void Test_Init_ThrowsIfListIsNull()
         {
             Assert.Throws<ArgumentNullException>(() => new BPMIntervalTracker(null));
         }
 
         [Test]
-        public void Test_Init_EmptyList()
+        public void Test_Init_WithEmptyList()
         {
             var list = new BeatObjectList<BPM>();
             var tracker = new BPMIntervalTracker(list);
@@ -25,7 +25,7 @@ namespace OpenChart.Tests.Charting
         }
 
         [Test]
-        public void Test_Init_NonEmptyList()
+        public void Test_Init_WithNonEmptyList()
         {
             var bpms = new BPM[] {
                 new BPM(100, 0),
@@ -54,17 +54,11 @@ namespace OpenChart.Tests.Charting
 
             var tracker = new BPMIntervalTracker(list);
 
-            try
-            {
-                var _ = tracker.Intervals;
-                Assert.Fail("Expected NoBPMAtBeatZeroException exception to be thrown.");
-            }
-            catch (NoBPMAtBeatZeroException)
-            { }
+            Assert.Throws<NoBPMAtBeatZeroException>(() => tracker.Intervals.GetValue(0));
         }
 
         [Test]
-        public void Test_Intervals_UpdatedWhenListChanged()
+        public void Test_Intervals_UpdatesWhenListIsChanged()
         {
             var list = new BeatObjectList<BPM>();
             var tracker = new BPMIntervalTracker(list);
@@ -80,6 +74,185 @@ namespace OpenChart.Tests.Charting
 
             list.Clear();
             Assert.IsEmpty(tracker.Intervals);
+        }
+
+        [Test]
+        public void Test_BeatToTime_ThrowsWhenListIsEmpty()
+        {
+            var list = new BeatObjectList<BPM>();
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.Throws<Exception>(() => tracker.BeatToTime(0));
+        }
+
+        [Test]
+        public void Test_BeatToTime_ThrowsWhenIndexIsOutOfRange()
+        {
+            var list = new BeatObjectList<BPM>();
+            list.Add(new BPM(100, 0));
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => tracker.BeatToTime(0, fromIndex: 1));
+        }
+
+        [Test]
+        public void Test_BeatToTime_ReturnsZeroForBeatZero()
+        {
+            var list = new BeatObjectList<BPM>();
+            list.Add(new BPM(99999, 0));
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(0, tracker.BeatToTime(0));
+        }
+
+        [Test]
+        public void Test_BeatToTime_ReturnsTimeAtIntervalStart()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[0].BeatToTime(0), tracker.BeatToTime(0));
+            Assert.AreEqual(tracker.Intervals[0].BeatToTime(10), tracker.BeatToTime(10));
+            Assert.AreEqual(tracker.Intervals[1].BeatToTime(15), tracker.BeatToTime(15));
+        }
+
+        [Test]
+        public void Test_BeatToTime_ReturnsTimeInBetweenIntervals()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[0].BeatToTime(5), tracker.BeatToTime(5));
+            Assert.AreEqual(tracker.Intervals[1].BeatToTime(12.5), tracker.BeatToTime(12.5));
+        }
+
+        [Test]
+        public void Test_BeatToTime_ReturnsTimeAfterLastInterval()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[2].BeatToTime(20), tracker.BeatToTime(20));
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ThrowsWhenListIsEmpty()
+        {
+            var list = new BeatObjectList<BPM>();
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.Throws<Exception>(() => tracker.TimeToBeat(0));
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ThrowsWhenIndexIsOutOfRange()
+        {
+            var list = new BeatObjectList<BPM>();
+            list.Add(new BPM(100, 0));
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => tracker.TimeToBeat(0, fromIndex: 1));
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ReturnsZeroForBeatZero()
+        {
+            var list = new BeatObjectList<BPM>();
+            list.Add(new BPM(99999, 0));
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(0, tracker.TimeToBeat(0).Value);
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ReturnsBeatAtIntervalStart()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[0].BPM.Beat.Value, tracker.TimeToBeat(tracker.Intervals[0].Seconds).Value);
+            Assert.AreEqual(tracker.Intervals[1].BPM.Beat.Value, tracker.TimeToBeat(tracker.Intervals[1].Seconds).Value);
+            Assert.AreEqual(tracker.Intervals[2].BPM.Beat.Value, tracker.TimeToBeat(tracker.Intervals[2].Seconds).Value);
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ReturnsBeatInBetweenIntervals()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[0].TimeToBeat(4).Value, tracker.TimeToBeat(4).Value);
+            Assert.AreEqual(tracker.Intervals[1].TimeToBeat(7).Value, tracker.TimeToBeat(7).Value);
+            Assert.AreEqual(tracker.Intervals[2].TimeToBeat(12.5).Value, tracker.TimeToBeat(12.5).Value);
+        }
+
+        [Test]
+        public void Test_TimeToBeat_ReturnsBeatAfterLastInterval()
+        {
+            var bpms = new BPM[] {
+                new BPM(100, 0),
+                new BPM(200, 10),
+                new BPM(300, 15),
+            };
+
+            var list = new BeatObjectList<BPM>();
+            list.Add(bpms[0]);
+            list.Add(bpms[1]);
+            list.Add(bpms[2]);
+
+            var tracker = new BPMIntervalTracker(list);
+
+            Assert.AreEqual(tracker.Intervals[2].TimeToBeat(20).Value, tracker.TimeToBeat(20).Value);
         }
     }
 }
