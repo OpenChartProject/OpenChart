fdb392ae5f8c8f7eee2df32e0ee99ffc2c5bfe80
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-01 22:07:01 -0400

    Add and rename interface

--- OpenChart/src/UI/Widgets/BeatLines.cs
+++ OpenChart/src/UI/Widgets/BeatLines.cs
@@ -2,8 +2,13 @@ using Gtk;
 
 namespace OpenChart.UI.Widgets
 {
-    public class BeatLines : DrawingArea
+    public class BeatLines : DrawingArea, INoteFieldWidget
     {
+        public NoteField NoteField { get; set; }
 
+        protected override bool OnDrawn(Cairo.Context cr)
+        {
+            return true;
+        }
     }
 }
--- OpenChart/src/UI/Widgets/IChartObject.cs
+++ OpenChart/src/UI/Widgets/IChartObject.cs
@@ -2,7 +2,7 @@ using OpenChart.Charting.Objects;
 
 namespace OpenChart.UI.Widgets
 {
-    public interface INoteFieldObject
+    public interface IChartObject
     {
         Gtk.Widget GetWidget();
         BaseObject GetChartObject();
