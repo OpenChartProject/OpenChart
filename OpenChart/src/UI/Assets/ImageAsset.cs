6cc480438567248fe908b3c5d529efc35dfeb6fb
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-03 17:04:23 -0400

    Add ScaleToWidth method for image assets

--- OpenChart/src/UI/Assets/ImageAsset.cs
+++ OpenChart/src/UI/Assets/ImageAsset.cs
@@ -1,4 +1,5 @@
 using Gdk;
+using System;
 using System.IO;
 
 namespace OpenChart.UI.Assets
@@ -19,10 +20,14 @@ namespace OpenChart.UI.Assets
         public readonly string Path;
 
         /// <summary>
-        /// A reusable Pixbuf instance.
+        /// The pixel buffer used by Gdk.
         /// </summary>
         public Pixbuf Pixbuf { get; private set; }
 
+        /// <summary>
+        /// Creates a new ImageAsset instance.
+        /// </summary>
+        /// <param name="path">The path to the image file.</param>
         public ImageAsset(string path)
         {
             Data = File.ReadAllBytes(path);
@@ -30,6 +35,12 @@ namespace OpenChart.UI.Assets
             Pixbuf = new Pixbuf(Data);
         }
 
+        /// <summary>
+        /// Creates a new ImageAsset instance.
+        /// </summary>
+        /// <param name="path">The path to the image file.</param>
+        /// <param name="width">The width to resize to (in pixels).</param>
+        /// <param name="height">The height to resize to (in pixels).</param>
         public ImageAsset(string path, int width, int height)
         {
             Data = File.ReadAllBytes(path);
@@ -37,9 +48,35 @@ namespace OpenChart.UI.Assets
             Pixbuf = new Pixbuf(Data, width, height);
         }
 
+        /// <summary>
+        /// Resizes the image to the provided width and height.
+        /// </summary>
+        /// <param name="width">The width to resize to (in pixels).</param>
+        /// <param name="height">The height to resize to (in pixels).</param>
         public void Resize(int width, int height)
         {
+            // FIXME: This creates a new pixbuf instance instead of resizing the current one.
+            // This means existing widgets using the image won't be resized. This method should
+            // resize and then copy the new data into this pixbuf, if possible.
             Pixbuf = new Pixbuf(Data, width, height);
+        }
+
+        /// <summary>
+        /// Scales the image width to the new width while keeping the aspect ratio the same.
+        /// </summary>
+        /// <param name="width">The width to scale the image to (in pixels).</param>
+        public void ScaleToWidth(int width)
+        {
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException("Width must be greater than zero.");
+            else if (width == Pixbuf.Width)
+                return;
+
+            // Maintain the aspect ratio by scaling the height by the same %.
+            var scalePercent = (double)Pixbuf.Width / width;
+            var newHeight = (int)Math.Round(Pixbuf.Height * scalePercent);
+
+            Resize(width, newHeight);
         }
     }
 }
