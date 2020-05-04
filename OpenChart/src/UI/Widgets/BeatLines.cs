6bb0e70eb64626f4f7f9a6de840f15afc5a0e327
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-03 22:15:09 -0400

    Make the first beat line for each measure thicc

--- OpenChart/src/UI/Widgets/BeatLines.cs
+++ OpenChart/src/UI/Widgets/BeatLines.cs
@@ -1,5 +1,5 @@
 using Gtk;
-using System;
+using System.Collections.Generic;
 
 namespace OpenChart.UI.Widgets
 {
@@ -10,12 +10,18 @@ namespace OpenChart.UI.Widgets
         protected override bool OnDrawn(Cairo.Context cr)
         {
             cr.SetSourceRGBA(1, 1, 1, 1);
-            cr.LineWidth = 1;
 
             var index = NoteField.ScrollIntervalIndex;
             var time = NoteField.ScrollTime;
             var iterator = NoteField.Chart.BPMList.Time.GetBeats(time, index);
             var beat = NoteField.ScrollBeat;
+
+            // The y-position of beat lines that mark the start of a measure. We're assuming
+            // 4/4 time so this will be every 4th beat.
+            var startOfMeasure = new List<int>();
+
+            // The y-position of beat lines that occur during a measure.
+            var duringMeasure = new List<int>();
 
             foreach (var beatTime in iterator)
             {
@@ -24,6 +30,30 @@ namespace OpenChart.UI.Widgets
                 if (y > NoteField.ViewportBottomY)
                     break;
 
+                if (beatTime.Beat.Value % 4 == 0)
+                    startOfMeasure.Add(y);
+                else
+                    duringMeasure.Add(y);
+            }
+
+            cr.SetSourceRGB(1, 1, 1);
+            cr.LineWidth = 3;
+
+            // Draw the measure start beat lines.
+            foreach (var y in startOfMeasure)
+            {
+                cr.MoveTo(0, y + 0.5);
+                cr.LineTo(AllocatedWidth, y + 0.5);
+            }
+
+            cr.Stroke();
+
+            cr.SetSourceRGB(0.65, 0.65, 0.65);
+            cr.LineWidth = 1;
+
+            // Draw the beat lines for during a measure.
+            foreach (var y in duringMeasure)
+            {
                 cr.MoveTo(0, y + 0.5);
                 cr.LineTo(AllocatedWidth, y + 0.5);
             }
