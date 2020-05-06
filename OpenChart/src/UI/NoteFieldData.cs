e2e5950e1795f9d0ce25ea8c2f17f5a3dc000a96
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-05 20:57:12 -0400

    Refactor scrolling beyond the chart bounds

--- OpenChart/src/UI/NoteFieldData.cs
+++ OpenChart/src/UI/NoteFieldData.cs
@@ -12,11 +12,12 @@ namespace OpenChart.UI
     public class NoteFieldData
     {
         /// <summary>
-        /// Represents the current scroll state of the note field.
+        /// Represents the current scroll state of the note field viewport.
         /// </summary>
         public class ScrollState
         {
             NoteFieldData data;
+            internal double positionOffset;
 
             /// <summary>
             /// The scroll position (in beats).
@@ -32,7 +33,7 @@ namespace OpenChart.UI
             /// <summary>
             /// The scroll position (in pixels).
             /// </summary>
-            public int Position => (int)Math.Floor(Time.Value * data.PixelsPerSecond);
+            public int Position => (int)Math.Round(positionOffset + Time.Value * data.PixelsPerSecond);
 
             /// <summary>
             /// The scroll position (in seconds).
@@ -49,6 +50,17 @@ namespace OpenChart.UI
                 Time = new Time(0);
             }
         }
+
+        /// <summary>
+        /// The raw number of input scroll "ticks" the note field has been scrolled by.
+        /// </summary>
+        double rawInputStepsScrolled;
+        int? oldViewportHeight;
+
+        /// <summary>
+        /// How much time 1 scroll step represents.
+        /// </summary>
+        int stepsPerSecond = 1;
 
         /// <summary>
         /// The Chart those note field is displaying.
@@ -71,32 +83,42 @@ namespace OpenChart.UI
         public int PixelsPerSecond { get; private set; }
 
         /// <summary>
-        /// The amount the note field has been scrolled by, in steps. 1 step = 1 mouse wheel tick.
-        /// NOTE: Use `ScrollTop.Time` instead of this to get the current scroll time.
-        /// </summary>
-        public double RawScrollPosition { get; private set; }
-
-        /// <summary>
         /// A multiplier for manipulating the scroll speed of the note field.
         /// </summary>
         public double ScrollScalar { get; private set; }
 
         /// <summary>
         /// The scroll state for the bottom of the note field viewport.
+        ///
+        /// <seealso cref="UpdateScroll" />
         /// </summary>
         public ScrollState ScrollBottom { get; private set; }
 
         /// <summary>
         /// The scroll state for the top of the note field viewport.
+        ///
+        /// <seealso cref="UpdateScroll" />
         /// </summary>
         public ScrollState ScrollTop { get; private set; }
+
+
+        /// <summary>
+        /// The amount of time to offset the note field components by. This is to allow for
+        /// a margin so that beat #0 is not right up against the edge of the widget.
+        /// </summary>
+        public Time TimeOffset { get; private set; }
+
+        /// <summary>
+        /// The time offset, in pixels.
+        /// </summary>
+        public int TimeOffsetPosition => (int)Math.Round(TimeOffset.Value * PixelsPerSecond);
 
         /// <summary>
         /// Creates a new NoteFieldData instance.
         /// </summary>
         /// <param name="chart">The chart this note field is displaying.</param>
         /// <param name="noteSkin">The note skin used by the note field.</param>
-        public NoteFieldData(Chart chart, KeyModeSkin noteSkin, int pixelsPerSecond)
+        public NoteFieldData(Chart chart, KeyModeSkin noteSkin, int pixelsPerSecond, Time timeOffset)
         {
             if (chart == null)
                 throw new ArgumentNullException("Chart cannot be null.");
@@ -105,11 +127,14 @@ namespace OpenChart.UI
             else if (pixelsPerSecond <= 0)
                 throw new ArgumentOutOfRangeException("Pixels per second must be greater than zero.");
 
+            oldViewportHeight = null;
+
             Chart = chart;
             NoteSkin = noteSkin;
 
             PixelsPerSecond = pixelsPerSecond;
             ScrollScalar = 0.25;
+            TimeOffset = timeOffset;
 
             ChartEvents = new ChartEventBus(Chart);
             ScrollBottom = new ScrollState(this);
@@ -136,36 +161,52 @@ namespace OpenChart.UI
         /// Updates the ScrollTop and ScrollBottom properties with the new scroll state
         /// of the note field widget.
         ///
-        /// Scrolling takes advantage of the fact that time is displayed linearly, unlike
-        /// BPMs which can change. Because time is displayed linearly we can easily map
-        /// between time and position.
+        /// ScrollTop and ScrollBottom represent the note field's position, relative to
+        /// the top and bottom of the viewport. The viewport is the visible part of the
+        /// widget on screen (i.e. the widget's size).
+        ///
+        /// To support margins so that the top and bottom of the note field aren't cut off,
+        /// you can actually scroll a bit past the note field in either direction, so long
+        /// as it stays on screen.
+        ///
+        /// One caveat with this is the fact that Beat and Time objects can't have a negative
+        /// value. So, for example, you can't technically scroll to a beat/time "before" the
+        /// beginning of the chart because the concept of a negative beat/time doesn't exist.
+        ///
+        /// The solution to this is to simply cap the Beat/Time, but still change the Position.
+        /// When scrolled before the beginning of the chart, the position is negative, but the
+        /// Beat/Time remain at 0. In a sense, what this is doing is pretending the viewport
+        /// is smaller than it really is when we're beyond the bounds of the chart.
         /// </summary>
         /// <param name="scrollDelta">The number of scroll steps the widget is scrolled.</param>
-        /// <param name="widgetHeight">The height of the widget (in pixels).</param>
-        /// <returns>True if the scroll position was updated.</returns>
-        public bool UpdateScroll(double scrollDelta, int widgetHeight)
+        /// <param name="viewportHeight">The height of the viewport (in pixels).</param>
+        /// <returns>True if the scroll position was changed.</returns>
+        public bool UpdateScroll(double scrollDelta, int viewportHeight)
         {
-            var oldPos = RawScrollPosition;
-            RawScrollPosition += scrollDelta * ScrollScalar;
+            var oldSteps = rawInputStepsScrolled;
+            rawInputStepsScrolled += scrollDelta * ScrollScalar;
 
-            if (RawScrollPosition < 0)
-                RawScrollPosition = 0;
+            if (rawInputStepsScrolled < 0)
+                rawInputStepsScrolled = 0;
 
-            // Nothing to update.
-            if (oldPos == RawScrollPosition)
+            // Nothing changed.
+            if (oldSteps == rawInputStepsScrolled && viewportHeight == oldViewportHeight)
                 return false;
 
-            // 1 step = 1 second.
-            var time = new Time(RawScrollPosition);
-            var y = time.Value * PixelsPerSecond;
+            oldViewportHeight = viewportHeight;
 
-            ScrollTop.Time = time;
+            var absTime = (rawInputStepsScrolled * stepsPerSecond) - TimeOffset.Value;
+
+            if (absTime < 0)
+                ScrollTop.positionOffset = absTime * PixelsPerSecond;
+            else
+                ScrollTop.positionOffset = 0;
+
+            ScrollTop.Time = Math.Max(absTime, 0);
             ScrollTop.IntervalIndex = Chart.BPMList.Time.GetIndexAtTime(ScrollTop.Time);
             ScrollTop.Beat = Chart.BPMList.Time.TimeToBeat(ScrollTop.Time, ScrollTop.IntervalIndex);
 
-            var bottomTime = new Time(time.Value + ((double)widgetHeight / PixelsPerSecond));
-
-            ScrollBottom.Time = bottomTime;
+            ScrollBottom.Time = ScrollTop.Time.Value + Math.Floor((double)viewportHeight / PixelsPerSecond);
             ScrollBottom.IntervalIndex = Chart.BPMList.Time.GetIndexAtTime(ScrollBottom.Time);
             ScrollBottom.Beat = Chart.BPMList.Time.TimeToBeat(ScrollBottom.Time, ScrollBottom.IntervalIndex);
 
