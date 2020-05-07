98233952625fd7f606fb9afe73eb1bdfe1e4aea8
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-05-06 20:33:37 -0400

    Add more comments, a couple TODOs

--- OpenChart/src/UI/Widgets/NoteField.cs
+++ OpenChart/src/UI/Widgets/NoteField.cs
@@ -5,12 +5,30 @@ using System.Collections.Generic;
 
 namespace OpenChart.UI.Widgets
 {
+    /// <summary>
+    /// This class is simply a container that wires everything up for the different components
+    /// of the note field to work. It's responsible for handling things like scrolling, and
+    /// handling/orchestrating chart events.
+    ///
+    /// The note field components themselves (BeatLines, NoteFieldKey, etc.) are meant to be
+    /// strictly presentational, meaning they don't really have any logic around knowing when
+    /// to update themselves. Hence, this class -- which contains those components -- can listen
+    /// to chart events and update the components accordingly.
+    /// </summary>
     public class NoteField : Layout
     {
+        /// <summary>
+        /// The container for <see cref="Keys" />
+        /// </summary>
         HBox keyContainer;
-        BeatLines beatLines;
         List<Widget> widgetStack;
 
+        /// <summary>
+        /// The background color of the note field.
+        ///
+        /// TODO: This exists mainly for testing purposes. The note field should have a transparent
+        /// background so we can show things like the audio wave form, chart bg image, etc.
+        /// </summary>
         public readonly RGBA BackgroundColor = new RGBA
         {
             Red = 0.1,
@@ -19,11 +37,30 @@ namespace OpenChart.UI.Widgets
             Alpha = 1.0,
         };
 
+        /// <summary>
+        /// The width of the note field widget.
+        /// </summary>
         public int NoteFieldWidth => NoteFieldData.KeyWidth * NoteFieldData.Chart.KeyCount.Value;
 
+        /// <summary>
+        /// The data/state for the note field.
+        /// </summary>
         public readonly NoteFieldData NoteFieldData;
+
+        /// <summary>
+        /// A widget for displaying beat lines.
+        /// </summary>
+        public readonly BeatLines BeatLines;
+
+        /// <summary>
+        /// The widgets for displaying chart objects.
+        /// </summary>
         public readonly NoteFieldKey[] Keys;
 
+        /// <summary>
+        /// Creates a new NoteField instance.
+        /// </summary>
+        /// <param name="noteFieldData">The data/state for the note field.</param>
         public NoteField(NoteFieldData noteFieldData) : base(null, null)
         {
             if (noteFieldData == null)
@@ -32,33 +69,34 @@ namespace OpenChart.UI.Widgets
             NoteFieldData = noteFieldData;
 
             widgetStack = new List<Widget>();
-            beatLines = new BeatLines(NoteFieldData);
+            BeatLines = new BeatLines(NoteFieldData);
             keyContainer = new HBox();
             Keys = new NoteFieldKey[NoteFieldData.Chart.KeyCount.Value];
 
+            // Create the NoteFieldKey widgets and add them to the container.
             for (var i = 0; i < NoteFieldData.Chart.KeyCount.Value; i++)
             {
                 Keys[i] = new NoteFieldKey(NoteFieldData, i);
                 keyContainer.Add(Keys[i]);
             }
 
-            beatLines.SetSizeRequest(NoteFieldWidth, NoteFieldData.ChartHeight);
+            // TODO: Size should be updated when the chart height changes.
+            BeatLines.SetSizeRequest(NoteFieldWidth, NoteFieldData.ChartHeight);
 
-            Add(beatLines);
+            Add(BeatLines);
             Add(keyContainer);
 
+            // Update visuals when scrolled or resized.
             ScrollEvent += onScroll;
-            SizeAllocated += (o, e) =>
-            {
-                NoteFieldData.UpdateScroll(0, e.Allocation.Height);
-            };
+            SizeAllocated += (o, e) => NoteFieldData.UpdateScroll(0, e.Allocation.Height);
 
-            NoteFieldData.ChartEvents.ObjectAdded += (o, e) =>
-            {
-                Keys[e.Object.KeyIndex.Value].Add(e.Object);
-            };
+            // Handle a new object being added to the chart.
+            NoteFieldData.ChartEvents.ObjectAdded += (o, e) => Keys[e.Object.KeyIndex.Value].Add(e.Object);
         }
 
+        /// <summary>
+        /// Adds the widget to the top of the widget stack.
+        /// </summary>
         public new void Add(Widget widget)
         {
             widgetStack.Add(widget);
@@ -71,6 +109,7 @@ namespace OpenChart.UI.Widgets
             Gdk.CairoHelper.SetSourceRgba(cr, BackgroundColor);
             cr.Paint();
 
+            // Render each widget in the stack in the order they were added.
             foreach (var widget in widgetStack)
             {
                 PropagateDraw(widget, cr);
@@ -81,6 +120,7 @@ namespace OpenChart.UI.Widgets
 
         private void onScroll(object o, ScrollEventArgs e)
         {
+            // Update the scroll state and return early if nothing was changed.
             if (!NoteFieldData.UpdateScroll(e.Event.DeltaY, AllocatedHeight))
                 return;
 
