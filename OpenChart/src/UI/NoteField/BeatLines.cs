e7a94ae6f4c0589c56521c68eefd1ede795aad27
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-06-05 20:14:54 -0400

    Add BeatLineSettings class

--- OpenChart/src/UI/NoteField/BeatLines.cs
+++ OpenChart/src/UI/NoteField/BeatLines.cs
@@ -1,19 +1,56 @@
+using Gtk;
+
 namespace OpenChart.UI.NoteField
 {
+    /// <summary>
+    /// The display settings for the BeatLines widget.
+    /// </summary>
+    public class BeatLineSettings
+    {
+        /// <summary>
+        /// The color of a beat line.
+        /// </summary>
+        public Color BeatLineColor { get; set; }
+
+        /// <summary>
+        /// The thickness (in pixels) of a beat line.
+        /// </summary>
+        public int BeatLineThickness { get; set; }
+
+        /// <summary>
+        /// The color for the start of a measure.
+        /// </summary>
+        public Color MeasureLineColor { get; set; }
+
+        /// <summary>
+        /// The thickness (in pixels) for the start of a measure.
+        /// </summary>
+        public int MeasureLineThickness { get; set; }
+    }
+
     /// <summary>
     /// Note field widget for displaying beat lines.
     /// </summary>
     public class BeatLines : IWidget
     {
+        public BeatLineSettings BeatLineSettings { get; private set; }
         public DisplaySettings DisplaySettings { get; private set; }
 
-        Gtk.DrawingArea drawingArea;
-        public Gtk.Widget GetWidget() => null;
+        DrawingArea drawingArea;
+        public Widget GetWidget() => drawingArea;
 
-        public BeatLines(DisplaySettings settings)
+        public BeatLines(DisplaySettings displaySettings, BeatLineSettings beatLineSettings)
         {
-            drawingArea = new Gtk.DrawingArea();
-            DisplaySettings = settings;
+            BeatLineSettings = beatLineSettings;
+            DisplaySettings = displaySettings;
+
+            drawingArea = new DrawingArea();
+            drawingArea.Drawn += onDrawn;
+        }
+
+        private void onDrawn(object o, DrawnArgs e)
+        {
+
         }
     }
 }
