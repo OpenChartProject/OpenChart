059dae32f27f631a0b4c7cc969f34b146c718c3b
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-06-07 16:45:11 -0400

    Add ImagePattern class

---
+++ OpenChart/src/UI/Assets/ImagePattern.cs
@@ -0,0 +1,39 @@
+using Cairo;
+
+namespace OpenChart.UI.Assets
+{
+    /// <summary>
+    /// Represents an image pattern. This is useful for when you need to tile an image.
+    /// </summary>
+    public class ImagePattern
+    {
+        /// <summary>
+        /// The image asset this pattern is displaying.
+        /// </summary>
+        public ImageAsset ImageAsset { get; private set; }
+
+        /// <summary>
+        /// The surface pattern. <seealso cref="Cairo.Context.SetSource(Surface)" />
+        /// </summary>
+        public SurfacePattern Pattern { get; private set; }
+
+        /// <summary>
+        /// The surface created from the image.
+        /// </summary>
+        public Surface Surface { get; private set; }
+
+        /// <summary>
+        /// Creates a new ImagePattern instance.
+        /// </summary>
+        /// <param name="image">The image asset to use.</param>
+        /// <param name="extendType">How the pattern should behave. Tile is the default.</param>
+        public ImagePattern(ImageAsset image, Extend extendType = Extend.Repeat)
+        {
+            ImageAsset = image;
+
+            Surface = Gdk.CairoHelper.SurfaceCreateFromPixbuf(ImageAsset.Pixbuf, 0, null);
+            Pattern = new SurfacePattern(Surface);
+            Pattern.Extend = extendType;
+        }
+    }
+}
