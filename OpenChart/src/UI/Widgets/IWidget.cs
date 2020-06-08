d581acb293002c9318e9239fab055afd8f7fa6fd
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-06-08 13:00:31 -0400

    Comment IWidget

--- OpenChart/src/UI/Widgets/IWidget.cs
+++ OpenChart/src/UI/Widgets/IWidget.cs
@@ -1,7 +1,13 @@
 namespace OpenChart.UI
 {
+    /// <summary>
+    /// Interface for a class that returns a Gtk widget.
+    /// </summary>
     public interface IWidget
     {
+        /// <summary>
+        /// Returns a Gtk widget.
+        /// </summary>
         Gtk.Widget GetWidget();
     }
 }
