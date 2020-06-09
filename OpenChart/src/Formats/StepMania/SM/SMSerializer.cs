dceb5a732c032bb3dabf3c7df4d3e6dc4dbd0e53
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-06-09 14:46:42 -0400

    Add a method for extracting .sm fields

--- OpenChart/src/Formats/StepMania/SM/SMSerializer.cs
+++ OpenChart/src/Formats/StepMania/SM/SMSerializer.cs
@@ -1,4 +1,6 @@
 using OpenChart.Formats.StepMania.SM.Data;
+using System.Collections.Generic;
+using System.Text;
 
 namespace OpenChart.Formats.StepMania.SM
 {
@@ -9,11 +11,28 @@ namespace OpenChart.Formats.StepMania.SM
     /// </summary>
     public class SMSerializer : IFormatSerializer<StepFileData>
     {
+        enum State
+        {
+            LookingForField,
+            ReadingName,
+            ReadingValue,
+            InComment,
+        }
+
+        const char TOKEN_COMMENT = '/';
+        const char TOKEN_FIELD_START = '#';
+        const char TOKEN_FIELD_NAME_END = ':';
+        const char TOKEN_FIELD_VALUE_END = ';';
+        const char TOKEN_NEWLINE = '\n';
+
         /// <summary>
         /// Deserializes raw file data into a StepFileData object.
         /// </summary>
         public StepFileData Deserialize(byte[] data)
         {
+            var strData = Encoding.UTF8.GetString(data);
+            var fields = ExtractFields(strData);
+
             return null;
         }
 
@@ -23,6 +42,84 @@ namespace OpenChart.Formats.StepMania.SM
         public byte[] Serialize(StepFileData obj)
         {
             return null;
+        }
+
+        /// <summary>
+        /// Extracts the raw fields from the step file. Returns a dictionary of the field names
+        /// and values. Field names are transformed to uppercase.
+        /// </summary>
+        public Dictionary<string, string> ExtractFields(string data)
+        {
+            var dict = new Dictionary<string, string>();
+            var state = State.LookingForField;
+            var preCommentState = state;
+
+            var buffer = new StringBuilder();
+            char last = '\0';
+            string name = "";
+
+            foreach (var c in data)
+            {
+                // Check for comments first. Save the state we were in so we can return to that
+                // state after the comment.
+                if (c == TOKEN_COMMENT && last == TOKEN_COMMENT)
+                {
+                    preCommentState = state;
+                    state = State.InComment;
+                    continue;
+                }
+
+                switch (state)
+                {
+                    // Looking for the start of a field. Skip until we hit a field start or find
+                    // a comment.
+                    case State.LookingForField:
+                        if (c == TOKEN_FIELD_START)
+                            state = State.ReadingName;
+
+                        break;
+
+                    // Reading the field name. Fill the buffer until we hit the separator.
+                    case State.ReadingName:
+                        if (c == TOKEN_FIELD_NAME_END)
+                        {
+                            // Save the field name.
+                            name = buffer.ToString().ToUpper();
+                            buffer.Clear();
+                            state = State.ReadingValue;
+                        }
+                        else
+                            buffer.Append(c);
+
+                        break;
+
+                    // Reading the field value. Fill the buffer until we hit the end of the field,
+                    // then add the field to the dictionary.
+                    case State.ReadingValue:
+                        if (c == TOKEN_FIELD_VALUE_END)
+                        {
+                            // Add the field to the dictionary.
+                            dict[name] = buffer.ToString();
+                            buffer.Clear();
+                            state = State.ReadingValue;
+                        }
+                        else
+                            buffer.Append(c);
+
+                        break;
+
+                    // Inside of a comment. Ignore the rest of the line.
+                    case State.InComment:
+                        if (c == TOKEN_NEWLINE)
+                            state = preCommentState;
+
+                        break;
+                }
+
+                last = c;
+            }
+
+            return dict;
         }
     }
 }
