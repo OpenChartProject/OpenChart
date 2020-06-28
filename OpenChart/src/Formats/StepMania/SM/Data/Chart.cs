1d88253bf6878bd64fe00848a4aaf5bda9c03e9a
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-06-28 19:52:33 -0400

    Add Chart data class

---
+++ OpenChart/src/Formats/StepMania/SM/Data/Chart.cs
@@ -0,0 +1,39 @@
+using OpenChart.Formats.StepMania.SM.Enums;
+
+namespace OpenChart.Formats.StepMania.SM.Data
+{
+    /// <summary>
+    /// Represents a chart in a step file.
+    /// </summary>
+    public class Chart
+    {
+        /// <summary>
+        /// A string describing what type of chart this is, e.g. 'dance-single'. I chose not to
+        /// make this an enum just because there are so many different types and they aren't
+        /// documented well (or at all).
+        /// </summary>
+        public string ChartType { get; set; }
+
+        /// <summary>
+        /// The author field.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// The chart's difficulty. This is what the user scrolls through to select the difficulty.
+        /// </summary>
+        public ChartDifficulty Difficulty { get; set; }
+
+        /// <summary>
+        /// The numeric difficulty. For example, if a chart is listed as an "18" on the song wheel.
+        /// </summary>
+        public int DifficultyRating { get; set; }
+
+        /// <summary>
+        /// Used by old versions of sm. Unused.
+        /// </summary>
+        public string GrooveRadarValues { get; set; }
+
+        // TODO: notedata
+    }
+}
