24a7e14470c329f9189d7a4d8d05db89ef61c41a
Jessie <2302541+Kangaroux@users.noreply.github.com>
2020-12-21 18:38:46 -0500

    Remove Renderer from SDL window

--- OpenChart/src/UI/Windows/SDLWindow.cs
+++ OpenChart/src/UI/Windows/SDLWindow.cs
@@ -11,11 +11,6 @@ namespace OpenChart.UI.Windows
         /// </summary>
         public readonly IntPtr WindowHandle;
 
-        /// <summary>
-        /// A pointer to the SDL renderer for the window.
-        /// </summary>
-        public readonly IntPtr Renderer;
-
         const string baseTitle = "OpenChart";
 
         const int initialWidth = 800;
@@ -37,17 +32,6 @@ namespace OpenChart.UI.Windows
             if (WindowHandle == null)
             {
                 var msg = string.Format("Failed to create window: %s", SDL.SDL_GetError());
-                Log.Fatal(msg);
-                throw new Exception(msg);
-            }
-
-            Renderer = SDL.SDL_CreateRenderer(WindowHandle, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
-
-            if (Renderer == null)
-            {
-                SDL.SDL_DestroyWindow(WindowHandle);
-
-                var msg = string.Format("Failed to create renderer: %s", SDL.SDL_GetError());
                 Log.Fatal(msg);
                 throw new Exception(msg);
             }
